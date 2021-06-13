47b8f2c38f4821b6dc4bd468cf73a2e27c1755c8
ryanb285 <73402247+ryanb285@users.noreply.github.com>
2021-06-13 18:54:48 -0400

    Static Data Pull
    will use alpha vantage API to pull data, need to update it to make it dynamic

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,1 +1,18 @@
-print("TO DO: make a web request for some stock market data")
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+symbol = "MSFT"
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+response = requests.get(request_url)
+
+print(type(response))
+
+print(response.text)
+
